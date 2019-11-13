f38ffe2871625c24087fcc2e2126d4dfb39892a0
stevenburris <stevenburris.jr@gmail.com>
2019-11-12 21:35:10 -0500

    making a list

--- program.c
+++ program.c
@@ -5,23 +5,39 @@ typedef char bool;
 #define true 1
 #define false 0
 #endif
+
+struct list_element
+{
+  struct list_element * next;
+};
+typedef struct list_element list_element;
+
+typedef struct
+{
+  list_element * first;
+  list_element * last;
+
+} list;
+
+void list_init(list * container)
+{
+  container->first = 0;
+  container->last = 0;
+}
+
+bool list_empty(list * container)
+{
+  return 0 == container->first;
+
+}
+
+list_element * list_begin(list * container)
+{
+  return container->first;
+}
+
+
 int main()
 {
-
-  bool c2f = false;
-  double c = 0;
-  double f = 86;
-
-  if (c2f)
-  {
-    f = c * 9 / 5 +32;
-  }
-  else
-  {
-    c = (f - 32) *5/9;
-  }
-
-  printf("%f C = %f F\n", c, f);
-
   return 0;
 }
