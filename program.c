4134acec8b84f25f58607ab2e3c905cccbbbe2cf
stevenburris <stevenburris.jr@gmail.com>
2019-11-13 09:41:45 -0500

    trying to figure out this list structure...

--- program.c
+++ program.c
@@ -63,13 +63,46 @@ list_element * list_pop_front(list * container)
   list_element * element = container->first;
   container->first = container->first->next;
   return element;
-  
+
 }
 
+#include <stdlib.h>
+#include <stdio.h>
 
+typedef struct
+{
+  list_element header;
+  int value;
+
+} apple;
 
 
 int main()
 {
+  list apples;
+  apple * a = (apple *) malloc(sizeof(apple));
+  apple * b = (apple *) malloc(sizeof(apple));
+  apple * c = (apple *) malloc(sizeof(apple));
+
+  a->value = 1;
+  b->value = 2;
+  c->value = 3;
+
+  list_init(&apples);
+
+  list_push_back(&apples, &a->header);
+  list_push_back(&apples, &b->header);
+  list_push_back(&apples, &c->header);
+
+  for (a = (apple *) list_begin(&apples); a; a = (apple *) list_next(&a->header))
+  {
+    printf("%d\n", a->value);
+  }
+
+  while (!list_empty(&apples)) {
+    a = (apple *) list_pop_front(&apples);
+    free(a);
+  }
+
   return 0;
 }
