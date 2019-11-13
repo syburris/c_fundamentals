b543921c402a167971023ab7c5e235ac2438b046
stevenburris <stevenburris.jr@gmail.com>
2019-11-12 21:45:34 -0500

    making a list

--- program.c
+++ program.c
@@ -36,6 +36,38 @@ list_element * list_begin(list * container)
   return container->first;
 }
 
+list_element * list_next(list * element)
+{
+  return element->next;
+}
+
+void list_push_back(list * container, list_element * element)
+{
+  if list_empty(container)
+  {
+    container->first = element;
+    container->last = element;
+  }
+  else
+  {
+    container->last->next = element;
+    container->last = element;
+  }
+
+  element->next = 0;
+
+}
+
+list_element * list_pop_front(list * container)
+{
+  list_element * element = container->first;
+  container->first = container->first->next;
+  return element;
+  
+}
+
+
+
 
 int main()
 {
